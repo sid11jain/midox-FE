883bbddf20b108425aa32601000b24519f1c2166
bilal74 <b.khalid13@gmail.com>
2023-05-18 14:43:46 +0530

    Comp created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'dashboard', loadChildren:()=>import('../dashboard/dashboard.module').then(mod=>mod.DashboardModule)
+    path:'dashboard', loadChildren:()=>import('./dashboard/dashboard.module').then(mod=>mod.DashboardModule)
   },
   {
-    path:'inventory', loadChildren:()=>import('../inventory/inventory.module').then(mod=>mod.InventoryModule)
+    path:'inventory', loadChildren:()=>import('./inventory/inventory.module').then(mod=>mod.InventoryModule)
   },
   {
-    path:'adda', loadChildren:()=>import('../adda/adda.module').then(mod=>mod.AddaModule)
+    path:'adda', loadChildren:()=>import('./adda/adda.module').then(mod=>mod.AddaModule)
   },
   {
-    path:'employee', loadChildren:()=>import('../employee/employee.module').then(mod=>mod.EmployeeModule)
+    path:'employee', loadChildren:()=>import('./employee/employee.module').then(mod=>mod.EmployeeModule)
   },
   {
-    path:'settings', loadChildren:()=>import('../settings/settings.module').then(mod=>mod.SettingsModule)
+    path:'settings', loadChildren:()=>import('./settings/settings.module').then(mod=>mod.SettingsModule)
   }
 ];
 
