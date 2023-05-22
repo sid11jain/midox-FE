74f2942377f261315a9b1d78b31df03d574ac6d8
mahendrsuthar <mahisuthar9529@gmail.com>
2023-05-22 14:18:15 +0530

    dashboard page changes

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { ChartConfiguration, ChartData, ChartDataset, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
@@ -16,10 +16,11 @@ export class DashboardPageComponent {
     responsive: true,
     plugins: {
       legend: {
-        display: true,
-        position: 'top',
+        display: false,
+        position: 'top'
       },
       datalabels: {
+        color: '#fff',
         formatter: (value:any, ctx:any) => {
           if (ctx.chart.data.labels) {
             return ctx.chart.data.labels[ctx.dataIndex];
@@ -29,9 +30,11 @@ export class DashboardPageComponent {
     }
   };
   public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
+    labels: [ [ 'Flopped' , '(3000)'], [ 'Booked' , '2000']],
     datasets: [ {
-      data: [ 300, 500, 100 ]
+      data: [ 300, 500 ],
+      //borderColor: '#36A2EB',
+      backgroundColor: ['#919ee5', '#3F51B5'],
     } ]
   };
   public pieChartType: ChartType = 'pie';
@@ -45,4 +48,63 @@ export class DashboardPageComponent {
   public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
     console.log(event, active);
   }
+
+  //bar chart
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DatalabelsPlugin
+  ];
+  public barChartData: ChartData<'bar'> = {
+    labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+    datasets: [
+      { 
+        data: [ 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56 ],
+        backgroundColor: [
+          '#3F51B5'
+        ],
+      },
+      
+    ],
+    
+  };
+  public barBundleChartData: ChartData<'bar'> = {
+    labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+    datasets: [
+      { 
+        data: [ 51, 26, 20, 18, 16, 51, 26, 20, 18, 16, 18, 22],
+        backgroundColor: [
+          '#3F51B5'
+        ],
+      },
+      
+    ],
+    
+  };
+  
+  public barChartLabels: any[] = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+  public barStackedChartData: ChartDataset[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56], label: 'Series A', stack: 'a', backgroundColor: ['#919ee5'] },
+    { data: [28, 48, 40, 19, 65, 59, 80, 81, 56, 86, 27, 90], label: 'Series B', stack: 'a' , backgroundColor: ['#3F51B5'],}
+  ];
+  
 }
