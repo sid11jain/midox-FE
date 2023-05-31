4e76d6b027f144eb4e8adc88d1be10d6efc4ee71
bilal74 <b.khalid13@gmail.com>
2023-06-01 00:13:38 +0530

    Add dispatch history, finish goods

--- src/app/inventory/dispatch-history/dispatch-history.component.ts
+++ src/app/inventory/dispatch-history/dispatch-history.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dispatch-history',
@@ -6,5 +6,254 @@ import { Component } from '@angular/core';
   styleUrls: ['./dispatch-history.component.scss']
 })
 export class DispatchHistoryComponent {
+  showSpinner:boolean = true;
 
+  ngOnInit(){
+    setTimeout(() => {
+      this.showSpinner = false
+    }, 1000);
+  }
+
+  viewFinishGoodsData: any = [
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+    {
+      unique_id:"123",
+      brand:"Ciana",
+      product:"T-shirt",
+      design:"C-1002",
+      dispatch_date:"13 Sept 2022",
+      avl_qty:1500
+    },
+];
 }
