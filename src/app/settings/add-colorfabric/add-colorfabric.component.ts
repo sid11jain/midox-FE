028353c5d8d096587e1efbc72f51fa5f662e37cb
bilal74 <b.khalid13@gmail.com>
2023-08-08 01:51:23 +0530

    Get api implemented in add color fabrcic

--- src/app/settings/add-colorfabric/add-colorfabric.component.ts
+++ src/app/settings/add-colorfabric/add-colorfabric.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-add-colorfabric',
@@ -9,13 +11,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddColorfabricComponent {
 
   colorFabricForm: FormGroup;
-  colorFabricData: any[] = [];
+  colorFabricData: any[] = []; 
+  colorFabricArray: any[] = []; 
   editedColorFabricIndex: number | null = null;
   deleteBtnDisabled: boolean = false;
   showDropdown: boolean = true;
+  showSpinner:boolean = true;
+  showSpinnerTable:boolean = true;
   dropDownValue:any = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private common: CommonService, public dialog: MatDialog) {
     this.colorFabricForm = this.formBuilder.group({
       name: ['', Validators.required],
       code: ['', Validators.required],
@@ -23,13 +28,17 @@ export class AddColorfabricComponent {
   }
   
   ngOnInit(){
-    this.dropDownValue = ["Red", "Green", "Brown", "Blue"];
+    this.getColorName();
+    this.getColorFabricCode();
   }
 
   onCheckboxChange(event: Event) {
     const target = event.target as HTMLInputElement;
     if(target.checked){
       this.showDropdown = false;
+      this.colorFabricForm.patchValue({
+        name: ""
+      });
     }
     else{
       this.showDropdown = true;
@@ -59,12 +68,42 @@ export class AddColorfabricComponent {
     this.deleteBtnDisabled = true;
     this.editedColorFabricIndex = index;
     this.colorFabricForm.patchValue({
-      name: data.name,
-      code: data.code
+      name: data.parentEntityCd,
+      code: data.displayValue
     });
   }
 
   delete(index: number) {
     this.colorFabricData.splice(index, 1);
+  }
+
+  //API Call Get color name
+  getColorName(){    
+    this.common.getAllSettingsData("MID_COL").subscribe((responseData:any)=>{
+      let response = responseData?.body;
+      if (responseData.status === 200) {
+        this.dropDownValue = response;
+        console.log(response);        
+      }
+      else{
+        console.log("Error code: ",responseData?.status);        
+      }
+      this.showSpinner = false;
+    });
+  }
+
+  //API Call Get color Fabric code
+  getColorFabricCode(){    
+    this.common.getAllSettingsData("MID_CFC").subscribe((responseData:any)=>{
+      let response = responseData?.body;
+      if (responseData.status === 200) {
+        this.colorFabricArray = response;
+        console.log(response);        
+      }
+      else{
+        console.log("Error code: ",responseData?.status);        
+      }
+      this.showSpinnerTable = false;
+    });
   }
 }
