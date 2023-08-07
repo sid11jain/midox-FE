e7e1a016bd2ce7f86d364b9b0068db4ae5c322ed
bilal74 <b.khalid13@gmail.com>
2023-08-08 02:00:40 +0530

    Get api implemented in add measurement type

--- src/app/settings/add-measurement/add-measurement.component.ts
+++ src/app/settings/add-measurement/add-measurement.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-add-measurement',
@@ -10,13 +12,19 @@ export class AddMeasurementComponent {
   
   measurementTypeForm: FormGroup;
   measurementTypeData: any[] = [];
+  measurementTypeArray: any[] = [];
   editedMeasurementTypeIndex: number | null = null;
   deleteBtnDisabled: boolean = false;
+  showSpinner: boolean = true;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private common: CommonService, public dialog: MatDialog) {
     this.measurementTypeForm = this.formBuilder.group({
       name: ['', Validators.required]
     });
+  }
+
+  ngOnInit(){
+    this.getMeasrurementType();
   }
 
   onSubmit() {
@@ -40,11 +48,26 @@ export class AddMeasurementComponent {
     this.deleteBtnDisabled = true;
     this.editedMeasurementTypeIndex = index;
     this.measurementTypeForm.patchValue({
-      name: data.name
+      name: data.displayValue
     });
   }
 
   delete(index: number) {
     this.measurementTypeData.splice(index, 1);
+  }
+
+  //API Call
+  getMeasrurementType(){    
+    this.common.getAllSettingsData("MID_UNIT").subscribe((responseData:any)=>{
+      let response = responseData?.body;
+      if (responseData.status === 200) {
+        this.measurementTypeArray = response;
+        console.log(response);        
+      }
+      else{
+        console.log("Error code: ",responseData?.status);        
+      }
+      this.showSpinner = false;
+    });
   }
 }
