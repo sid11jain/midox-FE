191c3e29566783d3481bac082cb3eedfa9500af5
bilal74 <b.khalid13@gmail.com>
2023-08-18 00:26:08 +0530

    Common fn added in add clr

--- src/app/settings/add-color/add-color.component.ts
+++ src/app/settings/add-color/add-color.component.ts
@@ -17,6 +17,7 @@ export class AddColorComponent {
   deleteBtnDisabled: boolean = false;
   showSpinner: boolean = true;
   
+  key: string = "MID_COL";
   dialogTitle: string = "Color";
   dialogMessage!: string;   
   editObject:any = {};
@@ -27,14 +28,17 @@ export class AddColorComponent {
     });
   }
 
-  ngOnInit(){
-    this.getColor();
+  async ngOnInit(){
+    // this.getColor();     
+    this.colorArray = await this.common.getDataFn(this.key);
+    this.showSpinner = false;
   }
   
-  onSubmit() {
+  async onSubmit() {
     if (this.colorForm.invalid) {
       return;
-    }
+    }    
+    this.showSpinner = true;
     
     this.deleteBtnDisabled = false;
     let inputVal = this.colorForm.value.name;
@@ -55,7 +59,9 @@ export class AddColorComponent {
       obj.displayValue = inputVal;
       // console.log("Obj ", obj);
       
-      this.editColor(obj);
+      // this.editColor(obj);      
+      this.colorArray = await this.common.editDataFn(obj,this.dialogTitle,this.key);
+      this.showSpinner = false;
     }
     else{
       let currTime = Date.now();
@@ -70,9 +76,13 @@ export class AddColorComponent {
       let data = [obj]
       console.log(data);    
 
-      this.addColor(data);
+      // this.addColor(data);
+      
+      // Post API call
+      this.colorArray = await this.common.addDataFn(data,this.dialogTitle,this.key);
+      this.showSpinner = false;
     }
-    this.showSpinner = true;
+    // this.showSpinner = true;
     
     this.editedColorIndex = null;
     this.colorForm.reset();
@@ -87,97 +97,101 @@ export class AddColorComponent {
     });
   }
 
-  delete(apiData: any) {    
+  async delete(apiData: any) {    
     this.showSpinner = true; 
     let entityCd = apiData?.entityCd;
-    this.deleteColor(entityCd);
+    // this.deleteColor(entityCd);
+    
+    // Delete API call
+    this.colorArray = await this.common.deleteDataFn(entityCd,this.dialogTitle,this.key);
+    this.showSpinner = false;
   }
 
   //API Call
-  getColor(){
-    console.log("API Call");
+  // getColor(){
+  //   console.log("API Call");
     
-    this.common.getAllSettingsData("MID_COL").subscribe((responseData:any)=>{
-      let response = responseData?.body;
-      if (responseData.status === 200) {
-        this.colorArray = response;
-        console.log(response);        
-      }
-      else{
-        console.log("Error code: ",responseData?.status);        
-      }
-      this.showSpinner = false;
-    });
-  }
+  //   this.common.getAllSettingsData("MID_COL").subscribe((responseData:any)=>{
+  //     let response = responseData?.body;
+  //     if (responseData.status === 200) {
+  //       this.colorArray = response;
+  //       console.log(response);        
+  //     }
+  //     else{
+  //       console.log("Error code: ",responseData?.status);        
+  //     }
+  //     this.showSpinner = false;
+  //   });
+  // }
   
   // Delete
-  deleteColor(entityCd:any){
-    console.log("API Call");    
-    this.common.deleteAllSettingsData(entityCd).subscribe((responseData:any)=>{
-      // let response = responseData?.body;
-      console.log(responseData);        
-      if (responseData.status === 200) {
-        this.getColor();
-        this.dialogMessage = 'Colour delete successfully.'; 
-      }
-      else{
-        console.log("Error code: ",responseData?.status); 
-        this.dialogMessage = 'Colour failed to delete.';        
-      }
-      this.showSpinner = false; 
-      this.openDialog();
-    });
-  }
+  // deleteColor(entityCd:any){
+  //   console.log("API Call");    
+  //   this.common.deleteAllSettingsData(entityCd).subscribe((responseData:any)=>{
+  //     // let response = responseData?.body;
+  //     console.log(responseData);        
+  //     if (responseData.status === 200) {
+  //       this.getColor();
+  //       this.dialogMessage = 'Colour delete successfully.'; 
+  //     }
+  //     else{
+  //       console.log("Error code: ",responseData?.status); 
+  //       this.dialogMessage = 'Colour failed to delete.';        
+  //     }
+  //     this.showSpinner = false; 
+  //     this.openDialog();
+  //   });
+  // }
 
   // Add
-  addColor(data:any){
-    console.log("Post API Call");
-    this.common.addAllSettingsData(data).subscribe((responseData:any)=>{
-      let response = responseData?.body;   
-      if (responseData.status === 201) {
-        console.log(response);       
-        this.getColor();    
+  // addColor(data:any){
+  //   console.log("Post API Call");
+  //   this.common.addAllSettingsData(data).subscribe((responseData:any)=>{
+  //     let response = responseData?.body;   
+  //     if (responseData.status === 201) {
+  //       console.log(response);       
+  //       this.getColor();    
         
-        this.dialogMessage = 'Colour saved successfully.'; 
-      }
-      else{
-        console.log("Error code: ",responseData?.status);    
-        this.dialogMessage = 'Colour failed to save.'; 
-      }      
-      this.showSpinner = false;  
-      this.openDialog();
-    });
-  }
+  //       this.dialogMessage = 'Colour saved successfully.'; 
+  //     }
+  //     else{
+  //       console.log("Error code: ",responseData?.status);    
+  //       this.dialogMessage = 'Colour failed to save.'; 
+  //     }      
+  //     this.showSpinner = false;  
+  //     this.openDialog();
+  //   });
+  // }
 
   // Edit
-  editColor(data:any){
-    console.log("Edit API Call");
-    this.common.editAllSettingsData(data).subscribe((responseData:any)=>{
-      let response = responseData?.body;   
-      if (responseData.status === 200) {
-        console.log(response);       
-        this.getColor();   
-        this.dialogMessage = 'Colour update successfully.'; 
-      }
-      else{
-        console.log("Error code: ",responseData?.status); 
-        this.dialogMessage = 'Colour failed to update.';        
-      }
-      this.showSpinner = false;  
-      this.openDialog();
-    });
-  }
+  // editColor(data:any){
+  //   console.log("Edit API Call");
+  //   this.common.editAllSettingsData(data).subscribe((responseData:any)=>{
+  //     let response = responseData?.body;   
+  //     if (responseData.status === 200) {
+  //       console.log(response);       
+  //       this.getColor();   
+  //       this.dialogMessage = 'Colour update successfully.'; 
+  //     }
+  //     else{
+  //       console.log("Error code: ",responseData?.status); 
+  //       this.dialogMessage = 'Colour failed to update.';        
+  //     }
+  //     this.showSpinner = false;  
+  //     this.openDialog();
+  //   });
+  // }
 
   //Modal 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(MsgDialogComponent, {
-      width: '400px',
-      data: { title: this.dialogTitle, message: this.dialogMessage }
-    });
+  // openDialog(): void {
+  //   const dialogRef = this.dialog.open(MsgDialogComponent, {
+  //     width: '400px',
+  //     data: { title: this.dialogTitle, message: this.dialogMessage }
+  //   });
 
-    dialogRef.afterClosed().subscribe((result:any) => {
-      console.log('The dialog was closed');
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((result:any) => {
+  //     console.log('The dialog was closed');
+  //   });
+  // }
 
 }
