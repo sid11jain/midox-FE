7aef288d2dd3cfd99626c2c7918c9c4ea4e04955
bilal74 <b.khalid13@gmail.com>
2023-09-20 13:36:46 +0530

    Accordian done on details page adda

--- src/app/adda/details-adda/adda-bundle/adda-bundle.component.ts
+++ src/app/adda/details-adda/adda-bundle/adda-bundle.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-adda-bundle',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./adda-bundle.component.scss']
 })
 export class AddaBundleComponent {
+  showSpinner:boolean = true;
+  bundleAddaData:any;  
+  employeeData:any = [];
 
+  constructor(private commonService: CommonService, private route: ActivatedRoute){  }
+
+  async ngOnInit(){    
+    await this.route.params.subscribe(async (params) => { 
+      const patternId = params['patternId'];
+      console.log('Received ID:', patternId);
+      this.bundleAddaData = await this.commonService.getDataFn1({"patternId":patternId}, "bundle", "get-bundles");
+      this.employeeData = await this.commonService.getDataFn1({}, "employee", "get-employees");
+      this.showSpinner = false;      
+      
+          
+    });
+    
+  }
 }
