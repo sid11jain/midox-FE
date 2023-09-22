b92e494e3d19925d47f00e454043afb5e7b95bd6
mahendrsuthar <mahisuthar9529@gmail.com>
2023-09-22 12:27:08 +0530

    pdf changes

--- src/app/adda/details-adda/adda-bundle/adda-bundle.component.ts
+++ src/app/adda/details-adda/adda-bundle/adda-bundle.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/services/common.service';
 import { ActivatedRoute } from '@angular/router';
 
-import * as pdfMake from "pdfmake/build/pdfmake";  
+import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from "pdfmake/build/vfs_fonts";
 (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
@@ -12,72 +12,345 @@ import * as pdfFonts from "pdfmake/build/vfs_fonts";
   styleUrls: ['./adda-bundle.component.scss']
 })
 export class AddaBundleComponent {
-  showSpinner:boolean = true;
-  bundleAddaData:any;  
-  employeeData:any = [];
-  processData:any = [];
+  showSpinner: boolean = true;
+  bundleAddaData: any;
+  employeeData: any = [];
+  processData: any = [];
 
-  constructor(private commonService: CommonService, private route: ActivatedRoute){  }
+  constructor(private commonService: CommonService, private route: ActivatedRoute) { }
 
-  async ngOnInit(){    
-    await this.route.params.subscribe(async (params) => { 
+  async ngOnInit() {
+    await this.route.params.subscribe(async (params) => {
       const patternId = params['patternId'];
       const brandId = params['brandId'];
       console.log('Received ID:', patternId);
-      this.bundleAddaData = await this.commonService.getDataFn1({"patternId":patternId}, "bundle", "get-bundles");
+      this.bundleAddaData = await this.commonService.getDataFn1({ "patternId": patternId }, "bundle", "get-bundles");
       this.employeeData = await this.commonService.getDataFn1({}, "employee", "get-employees");
-      this.processData = await this.commonService.getDataFn1({"brandId":brandId}, "design", "get-designs");
+      this.processData = await this.commonService.getDataFn1({ "brandId": brandId }, "design", "get-designs");
       this.processData = this.processData[0].processes;
       console.log("processData ", this.processData);
-      
-      this.showSpinner = false;      
-      
-          
+
+      this.showSpinner = false;
+
+
     });
-    
+
   }
 
-  exportPdf(): void {
-
-    const docDefinition:any = {
+  exportPdf(data:any): void {
+    let detail = data?.data;
     
+    const docDefinition: any =  {
+      pageOrientation: "portrait",
+      //pageMargins: [20, 10, 20, 40],
+      pageSize: "A6",
+    //   header: {
+    //     margin: 25.5,
+    //     columns: [
+    //         {
+    //             image: logo,
+    //             height: 106.4,
+    //             width: 540
+    //             // alignment: 'center'
+    //         }
+    //     ]
+    // },
+
+    footer: function (currentPage:any, pageCount:any) {
+      return [{ text: 'Page ' + currentPage.toString() + ' of ' + pageCount, alignment: 'center' }];
+    },
+      
       content: [
-        { text: "Tables", style: "header" },
-        "Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.",
+        { text: "JC-2713", style: "header" },
         {
-          text:
-            "A simple table (no headers, no width specified, no spans, no styling)",
+            
+          canvas: [
+              { type: 'line', x1: 0, y1: -15, x2: 520, y2: -15, lineWidth: 2 }, //Bottom line
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              // columns: [
+              //   {
+              //     text: [{text:"Employee", bold:true},{text:":     "}, {text:"Pooja"}],
+              //   }
+              // ],
+              columns: [
+                {
+                  text: "Employee",
+                  bold: true
+                },
+                {
+                  text: detail?.createdBy?.empName
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Job Assigned",
+                  bold: true
+                },
+                {
+                  text: detail?.currentProcessCd?.displayValue
+                }
+              ]
+            }
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Adda No.",
+                  bold: true
+                },
+                {
+                  text: detail?.currentProcessCd?.displayValue
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Item",
+                  bold: true
+                },
+                {
+                  text: "F4"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Product",
+                  bold: true
+                },
+                {
+                  text: detail?.bundleName
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Size",
+                  bold: true
+                },
+                {
+                  text: detail?.bundleName
+                }
+              ]
+            }
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Bundle",
+                  bold: true
+                },
+                {
+                  text: detail?.bundleName
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Design",
+                  bold: true
+                },
+                {
+                  text: detail?.currentProcessCd?.displayValue
+                }
+              ]
+            }
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Quantity",
+                  bold: true
+                },
+                {
+                  text: detail?.quantity
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Wage to Provide",
+                  bold: true
+                },
+                {
+                  text: detail?.quantity
+                }
+              ]
+            }
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Rate",
+                  bold: true
+                },
+                {
+                  text: detail?.quantity
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+                {
+                  text: "Job Flow Number",
+                  bold: true
+                },
+                {
+                  text: detail?.currentProcessCd?.displayValue
+                }
+              ]
+            }
+          ]
+        },
+        {
+          text: "Job Details",
           style: "subheader"
         },
-        "The following table has nothing more than a body array",
         {
-          style: "tableExample",
-          table: {
-            body: [
-              ["Column 1", "Column 2", "Column 3"],
-              ["One value goes here", "Another one here", "OK?"]
-            ]
-          }
-        }
+            
+          canvas: [
+              { type: 'line', x1: 0, y1: -15, x2: 520, y2: -15, lineWidth: 1 }, //Bottom line
+          ]
+        },
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Product Job",
+                  bold: true
+                },
+                {
+                  text: detail?.displayValue
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+
+              ]
+            }
+          ]
+        },
+
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Status",
+                  bold: true
+                },
+                {
+                  text: detail?.status?.displayValue
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+
+              ]
+            }
+          ]
+        },
+
+        {
+          alignment: 'justify',
+          style: "row",
+          columns: [
+            {
+              columns: [
+                {
+                  text: "Last Modified By",
+                  bold: true
+                },
+                {
+                  text: detail?.updatedBy?.empName
+                }
+              ],
+              style: "column",
+            },
+            {
+              columns: [
+
+              ]
+            }
+          ]
+        },
       ],
 
       styles: {
         header: {
-          fontSize: 18,
+          fontSize: 20,
           bold: true,
-          margin: [0, 0, 0, 10]
+          margin: [0, 0, 0, 20]
         },
         subheader: {
-          fontSize: 16,
+          fontSize: 15,
           bold: true,
-          margin: [0, 10, 0, 5]
+          margin: [0, 30, 0, 20],
+          fillColor: '#cccccc'
         },
-        tableExample: {
-          margin: [0, 5, 0, 15]
+        row: {
+          margin: [20,0, 0, 0],
+          fontSize: 10,
+        },
+        column: {
+          margin: [10, 0, 10, 10]
         }
       }
     };
-
     pdfMake.createPdf(docDefinition).open();
   }
 }
